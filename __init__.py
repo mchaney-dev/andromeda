689398f6eb90cc21752a4ed169af9ae847890bec
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2022-12-20 09:50:02 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from utils import save_model_state, load_model_state, log
-from model import Model
+import model
 import os
 
 log('Initializing...')
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 if not os.path.exists('andromeda_state.pkl'):
-    andromeda = Model()
+    andromeda = model.Model()
     log('Saving model state...')
     save_model_state(andromeda, 'andromeda')
     log('Done.')
