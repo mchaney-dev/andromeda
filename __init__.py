1109418d2c4db48db540a81e6bc2d733ec556c91
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2022-12-20 09:57:09 -0500

    fixed imports

--- __init__.py
+++ __init__.py
@@ -1,14 +1,1 @@
-from utils import save_model_state, load_model_state, log
-import model
-import os
-
-log('Initializing...')
-os.chdir(os.path.abspath(os.path.dirname(__file__)))
-if not os.path.exists('andromeda_state.pkl'):
-    andromeda = model.Model()
-    log('Saving model state...')
-    save_model_state(andromeda, 'andromeda')
-    log('Done.')
-log('Loading model state...')
-andromeda = load_model_state('andromeda')
-log('Done.')
+from . import model
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from happytransformer import HappyGeneration, GENSettings, GENTrainArgs
 import os
+from utils import save_model_state, load_model_state, log
 
 class Model(HappyGeneration):
     def __init__(self):
@@ -9,12 +10,18 @@ class Model(HappyGeneration):
         self.version = '0.0.1'
         self.path = f'{cwd}/andromeda-latest'
         if not os.path.exists(self.path):
+            log('Initializing...')
             os.mkdir(self.path)
             os.chdir(self.path)
             os.mkdir(f'{self.path}/training')
             os.mkdir(f'{self.path}/training/checkpoints')
             os.mkdir(f'{self.path}/training/data')
-            self.model = HappyGeneration(model_type='GPT-NEO', model_name='EleutherAI/gpt-neo-125M')
-            self.model.save(self.path)
-        else:
+            _model = HappyGeneration(model_type='GPT-NEO', model_name='EleutherAI/gpt-neo-125M')
+            _model.save(self.path)
             self.model = HappyGeneration(model_type='GPT-NEO', model_name='EleutherAI/gpt-neo-125M', load_path=self.path)
+            log('Saving model state...')
+            save_model_state(self.model, 'andromeda')
+            log('Done.')
+        log('Loading model state...')
+        self.model = load_model_state('andromeda')
+        log('Done.')
