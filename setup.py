902063cdd3ed5e281f75a5f26e63d49ccd043cb8
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2023-01-25 15:28:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ import setuptools
 
 setuptools.setup(
     name='andromeda-nlp',
-    version='0.1.3',
+    version='0.1.2',
     description='Easy longform text generation for creative writing.',
     long_description='Easy longform text generation for creative writing.',
     author='mchaney-dev',
     author_email='mchaneydev@gmail.com',
     url='https://github.com/mchaney-dev/andromeda',
-    install_requires=['torch', 'happytransformer', 'datetime', 'typing'],
-    packages=setuptools.find_packages(),
+    requires=['torch', 'happytransformer', 'datetime', 'typing'],
     license_files=['LICENSE']
 )
