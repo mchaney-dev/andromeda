9060bb2da48b304e3e77d6b6994881f2f0f0138d
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2023-01-30 09:14:51 -0500

    Revert "Update setup.py"
    This reverts commit 902063cdd3ed5e281f75a5f26e63d49ccd043cb8.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='andromeda-nlp',
-    version='0.1.2',
+    version='0.1.3',
     description='Easy longform text generation for creative writing.',
     long_description='Easy longform text generation for creative writing.',
     author='mchaney-dev',
     author_email='mchaneydev@gmail.com',
     url='https://github.com/mchaney-dev/andromeda',
-    requires=['torch', 'happytransformer', 'datetime', 'typing'],
+    install_requires=['torch', 'happytransformer', 'datetime', 'typing'],
+    packages=setuptools.find_packages(),
     license_files=['LICENSE']
 )
